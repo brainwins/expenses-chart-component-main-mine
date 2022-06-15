8cb436527f9fcce583510e67f80660f1a13c390c
unknown <shofondo@gmail.com>
2022-06-15 12:39:58 -0500

    mobile ready

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,28 @@
-//
+const date = new Date();
+const today = date.getDay();
+
+const daysOfWeekLabel = {
+    0: 'mon',
+    1: 'tue',
+    2: 'wed',
+    3: 'thu',
+    4: 'fri',
+    5: 'sat',
+    6: 'sun'
+};
+
+const daysOfWeekJs = {
+  0: 'sun',
+  1: 'mon',
+  2: 'tue',
+  3: 'wed',
+  4: 'thu',
+  5: 'fri',
+  6: 'sat'
+}
+
+
+//Get data from JSON
 async function getData() {
     let url = './js/data.json';
     try {
@@ -9,32 +33,39 @@ async function getData() {
     }
 }
 
+//Generate DIVS
 async function renderData() {
-    let data = await getData();
-    data.forEach((element, i) => {
-      const targetbar = document.querySelector(`[data-day="${i}"]`);
-      const targetBarHover = document.querySelector(`[data-bar-hover="${i}"]`);
-      targetbar.style.paddingTop = `${element.amount*2.75}px`;
-      targetBarHover.innerHTML = "$" + element.amount;
+  let data = await getData();
+  data.forEach((element, i) => {
 
-    });
+    const divDay = document.createElement("div");
+    divDay.classList.add('day');
 
+    const divLabel = document.createElement("div");
+    divLabel.classList.add('label');
+    let labelText = document.createTextNode(daysOfWeekLabel[i]);
+    divLabel.appendChild(labelText);
+    divDay.appendChild(divLabel)
 
+    const divBar = document.createElement("div");
+    divBar.classList.add('bar');
+    divBar.style.paddingTop = `${element.amount*2.75}px`;
+    //color cyan if today
+    if (daysOfWeekJs[today] == daysOfWeekLabel[i]) {
+      divBar.style.backgroundColor = 'hsl(186, 34%, 60%)';
+    }
+    divDay.appendChild(divBar);
+
+    const divHover = document.createElement("div");
+    divHover.classList.add('bar-hover');
+    divHoverText = document.createTextNode("$" + element.amount);
+    divHover.appendChild(divHoverText);
+    divDay.appendChild(divHover);
+
+    document.querySelector(".graph-bars").appendChild(divDay);
+
+  });
 
 }
 
 renderData();
-
-//Color today's bar cyan
-const date = new Date();
-const today = date.getDay()
-const todaysBar = document.querySelector(`[data-day="${today}"]`)
-todaysBar.style.backgroundColor = 'hsl(186, 34%, 60%)';
-
-/*
-bar = document.querySelector(".bar");
-bar.addEventListener('mouseover', event => {
-  console.log(bar.nextElementSibling.style.display="block")
-    //Handleclick(item);
-  })
-*/
